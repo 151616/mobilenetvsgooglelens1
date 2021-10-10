38f5b2cd8e111f4197a62889a3085072673983b5
shivansh payak <75551006+151616@users.noreply.github.com>
2021-10-10 18:15:02 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,51 @@
+var album = ["water_bottle.png",
+"bowl_prev_ui.png", 
+"comb.png", 
+"light-bulb-png-bulb-png1243-0-removebg-preview.png", 
+"unnamed-removebg-preview.png", 
+"phone.png", 
+"alarm_clock.png", 
+"padlock.png", 
+"191-1916611_logo-clipart-chargers-mobile-charger-png-transparent-png-removebg-preview.png", 
+"headphones.png"];
+
+function water_bottle_div() {
+    document.getElementById("main_image_div").src = album[0];
+    document.getElementById("main_div").style.display = "initial";
+}
+function bowl_div(){
+    document.getElementById("main_image_div").src = album[1];
+    document.getElementById("main_div").style.display = "initial";
+}
+function comb_div(){
+    document.getElementById("main_image_div").src = album[2];
+    document.getElementById("main_div").style.display = "initial";
+}
+function light_bulb_div(){
+    document.getElementById("main_image_div").src = album[3];
+    document.getElementById("main_div").style.display = "initial";
+}
+function glass_div(){
+    document.getElementById("main_image_div").src = album[4];
+    document.getElementById("main_div").style.display = "initial";
+}
+function phone_div(){
+    document.getElementById("main_image_div").src = album[5];
+    document.getElementById("main_div").style.display = "initial";
+}
+function clock_div(){
+    document.getElementById("main_image_div").src = album[6];
+    document.getElementById("main_div").style.display = "initial";
+}
+function padlock_div(){
+    document.getElementById("main_image_div").src = album[7];
+    document.getElementById("main_div").style.display = "initial";
+}
+function charger_div(){
+    document.getElementById("main_image_div").src = album[8];
+    document.getElementById("main_div").style.display = "initial";
+}
+function headphones_div(){
+    document.getElementById("main_image_div").src = album[9];
+    document.getElementById("main_div").style.display = "initial";
+}
